af24b87b8278babb26959c3cce1029a0f7520ae9
Roberto <rgg.correo@gmail.com>
2017-10-31 20:37:01 -0600

    Initial Commit

---
+++ DronCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DronCamera
+//
+//  Created by Roberto Gervacio on 10/31/17.
+//  Copyright © 2017 Roberto Gervacio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DronCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DronCamera
+//
+//  Created by Roberto Gervacio on 10/31/17.
+//  Copyright © 2017 Roberto Gervacio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
