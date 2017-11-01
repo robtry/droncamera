3479cc13589f729484ba5f5009126bf0e2e477e8
Roberto <rgg.correo@gmail.com>
2017-10-31 21:29:06 -0600

    Agregando libreria de video

---
+++ DronCamera/DJICameraViewController.m
@@ -0,0 +1,60 @@
+//
+//  DJICameraViewController.m
+//  DronCamera
+//
+//  Created by Roberto Gervacio on 10/31/17.
+//  Copyright © 2017 Roberto Gervacio. All rights reserved.
+//
+#import "DJICameraViewController.h"
+#import <DJISDK/DJISDK.h>
+
+@interface DJICameraViewController ()<DJISDKManagerDelegate>
+
+@end
+
+@implementation DJICameraViewController
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    [self registerApp];
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)registerApp
+{
+    [DJISDKManager registerAppWithDelegate:self];
+}
+
+#pragma mark DJISDKManager Delegate Method
+- (void)appRegisteredWithError:(NSError *)error
+{
+    NSString* message = @"Register App Successed!";
+    if (error) {
+        message = @"Register App Failed! Please enter your App Key in the plist file and check the network.";
+    }else
+    {
+        NSLog(@"registerAppSuccess");
+    }
+    
+    [self showAlertViewWithTitle:@"Register App" withMessage:message];
+}
+
+- (void)showAlertViewWithTitle:(NSString *)title withMessage:(NSString *)message
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:okAction];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
